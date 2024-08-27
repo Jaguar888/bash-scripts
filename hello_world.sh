cf3e04bef8908289872de96c9aed774d0b4c0e5d
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-27 19:06:42 -0400

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -3,8 +3,8 @@
 yes | sudo apt install openssh-server
 yes | sudo apt install openssh-client
 sudo ufw enable
-sudo cp -f bronny.txt /etc/apt/sources.list #settings for updates
-sudo cp -f lebron.txt /etc/login.defs #password max age, warn, min change
-sudo cp -f giannis.txt /etc/pam.d/common-auth #min length for password
-sudo cp -f bron.txt /etc/ssh/sshd_config #permit root login no
-sudo find /home -mindepth 3 -delete
+sudo cp -f IdealSecuritySettings.txt /etc/apt/sources.list #settings for updates
+sudo cp -f PassAgeWarnChange.txt /etc/login.defs #password max age, warn, min change
+sudo cp -f PassMinLen.txt /etc/pam.d/common-auth #min length for password
+sudo cp -f RootLoginDenial.txt /etc/ssh/sshd_config #permit root login no
+
