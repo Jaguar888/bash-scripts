70d18c00fef4dfe5e3b0d7d1d56f08dcd7ff9609
dovahkiin <sidsingh.8778@gmail.com>
2024-08-29 02:49:25 +0000

    average updates

--- change_pass.sh
+++ change_pass.sh
@@ -1,5 +1,8 @@
+set -e
+sudo apt install openssl
 users=$(cat /etc/passwd | cut -d ":" -f1)
-for user in users:
-  args = "$username"
-  args += ":Cyb3rCont3st!"
-  sudo chpass "$args"
+for user in $users; do
+        pass = "Cyb3rCont3st!"
+	sudo usermod --password $(echo "$pass" | openssl passwd -1 -stdin) $user
+echo "Program Complete. Password set to Cyb3rCont3st!"
+done
