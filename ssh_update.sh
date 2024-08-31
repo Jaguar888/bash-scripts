e9d488340f09f5c28bf26f34b7062f1b87fd056f
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-31 14:07:43 -0400

    Update ssh_update.sh

--- ssh_update.sh
+++ ssh_update.sh
@@ -1,17 +1,16 @@
-$current_dir='pwd'
-$current_dir+='/SSHDSetup.txt'
-echo $current_dir
-wget https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.8p1.tar.gz
-sudo systemctl stop sshd
-yes | sudo apt-get remove openssh-server openssh-client
 sudo apt update
-sudo apt install -y build-essential zlib1g-dev libssl-dev libpam0g-dev libselinux1-dev libwrap0-dev libedit-dev libbsd-dev autoconf automake libtool pkg-config wget curl git
-tar zxvf openssh-9.8p1.tar.gz
-cd openssh-9.8p1
-./configure
+sudo apt install build-essential zlib1g-dev libssl-dev
+sudo mkdir /var/lib/sshd
+sudo chmod -R 700 /var/lib/sshd/
+sudo chown -R root:sys /var/lib/sshd/
+wget -c https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.8p1.tar.gz
+tar -xzf openssh-8.0p1.tar.gz
+cd openssh-8.0p1/
+sudo apt install libpam0g-dev libselinux1-dev
+./configure --with-md5-passwords --with-pam --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh
 make
-yes | sudo make install
-cp -f SSHDSetup.txt /etc/systemd/system/sshd.service
+sudo make install
+
 sudo systemctl daemon-reload
 sudo systemctl start sshd
 sudo systemctl enable sshd
@@ -19,3 +18,5 @@ sudo systemctl unmask ssh
 sudo systemctl daemon-reload
 sudo systemctl start sshd
 sudo systemctl enable sshd
+sudo systemctl enable ssh
+sudo systemctl start ssh
