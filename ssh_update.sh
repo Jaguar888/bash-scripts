73eb5f8efe81c8bfa687376bd3277ba0c87e3e4d
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-31 13:30:19 -0400

    Update ssh_update.sh

--- ssh_update.sh
+++ ssh_update.sh
@@ -1,1 +1,19 @@
-#in progress
+yes | sudo make install
+wget https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.8p1.tar.gz
+sudo systemctl stop sshd
+sudo apt-get remove openssh-server openssh-client
+sudo apt update
+sudo apt install -y build-essential zlib1g-dev libssl-dev libpam0g-dev libselinux1-dev libwrap0-dev libedit-dev libbsd-dev autoconf automake libtool pkg-config wget curl git
+tar zxvf openssh-9.8p1.tar.gz
+cd openssh-9.8p1
+./configure
+make
+sudo nano /etc/systemd/system/sshd.service
+cp -f SSHDSetup.txt /etc/systemd/system/sshd.service
+sudo systemctl daemon-reload
+sudo systemctl start sshd
+sudo systemctl enable sshd
+sudo systemctl unmask ssh
+sudo systemctl daemon-reload
+sudo systemctl start sshd
+sudo systemctl enable sshd
