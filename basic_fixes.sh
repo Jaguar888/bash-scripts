db855a0fc5dd75170f67b2e1386155853689b846
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-31 17:15:18 -0400

    Update basic_fixes.sh

--- basic_fixes.sh
+++ basic_fixes.sh
@@ -8,4 +8,5 @@ sudo cp -f PassAgeWarnChange.txt /etc/login.defs #password max age, warn, min ch
 sudo cp -f PassMinLen.txt /etc/pam.d/common-password #min length for password
 sudo cp -f RootLoginDenial.txt /etc/ssh/sshd_config #permit root login no
 sudo cp -f SetLockout.txt /etc/pam.d/common-auth #permit root login no
-
+yes | sudo apt install openssh-server
+yes | sudo apt install openssh-client
