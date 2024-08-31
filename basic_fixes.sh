677260089218826fb910294d44c621ba1dc63829
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-30 21:20:40 -0400

    Update basic_fixes.sh

--- basic_fixes.sh
+++ basic_fixes.sh
@@ -2,7 +2,9 @@
 
 yes | sudo apt install openssh-server
 yes | sudo apt install openssh-client
+yes | sudo apt install openssl
 sudo ufw enable
+sudo bash change_pass.sh
 sudo cp -f IdealSecuritySettings.txt /etc/apt/sources.list #settings for updates
 sudo cp -f PassAgeWarnChange.txt /etc/login.defs #password max age, warn, min change
 sudo cp -f PassMinLen.txt /etc/pam.d/common-password #min length for password
