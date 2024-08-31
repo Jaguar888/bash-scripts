d2b3598262f81f049f46c2cffd09a3d2fff81fb7
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-30 21:19:02 -0400

    Update change_pass.sh

--- change_pass.sh
+++ change_pass.sh
@@ -1,8 +1,9 @@
-set -e
-sudo apt install openssl
+echo "New Password is zxcvb!@#$"
 users=$(cat /etc/passwd | cut -d ":" -f1)
-for user in $users; do
-        pass = "Cyb3rCont3st!"
-	sudo usermod --password $(echo "$pass" | openssl passwd -1 -stdin) $user
-echo "Program Complete. Password set to Cyb3rCont3st!"
+for user in $users 
+do
+pass="zxcvb!@#$"
+sudo usermod --password $(echo $pass | openssl passwd -1 -stdin) $user
+
 done
+
