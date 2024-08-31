85586b87368f8cde9a7392e77cf02413e990d680
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-30 20:59:11 -0400

    Update basic_fixes.sh

--- basic_fixes.sh
+++ basic_fixes.sh
@@ -7,4 +7,5 @@ sudo cp -f IdealSecuritySettings.txt /etc/apt/sources.list #settings for updates
 sudo cp -f PassAgeWarnChange.txt /etc/login.defs #password max age, warn, min change
 sudo cp -f PassMinLen.txt /etc/pam.d/common-auth #min length for password
 sudo cp -f RootLoginDenial.txt /etc/ssh/sshd_config #permit root login no
+sudo cp -f SetLockout.txt /etc/pam.d/common-auth #permit root login no
 
