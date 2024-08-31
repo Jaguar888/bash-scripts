2b86de9f45cdb9b14a1287cac01381cfcfce0488
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-08-31 13:22:06 -0400

    Update ssh_update.sh

--- ssh_update.sh
+++ ssh_update.sh
@@ -1,8 +1,1 @@
-sudo apt purge openssh-client -y
-sudo apt purge openssh-server -y
-sudo apt update
-sudo apt upgrade
-yes | sudo apt install openssh-server
-yes | sudo apt install openssh-client
-sudo systemctl enable ssh
-sudo systemctl start ssh
+#in progress
