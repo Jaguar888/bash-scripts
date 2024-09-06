5017654727ac8274ecb20dc3c2aae6ea60fde96e
Jaguar888 <91759932+Jaguar888@users.noreply.github.com>
2024-09-06 19:51:13 -0400

    Update basic_fixes.sh

--- basic_fixes.sh
+++ basic_fixes.sh
@@ -7,8 +7,8 @@ sudo bash IndivScripts/change_pass.sh
 sudo cp -f Ideals/IdealSecuritySettings.txt /etc/apt/sources.list #settings for>
 sudo cp -f Ideals/PassAgeWarnChange.txt /etc/login.defs #password max age, warn>
 sudo cp -f Ideals/PassMinLen.txt /etc/pam.d/common-password #min length for pas>
-sudo cp -f Ideals/RootLoginDenial.txt /etc/ssh/sshd_config #permit root login no
-sudo cp -f Ideals/SetLockout.txt /etc/pam.d/common-auth #permit root login no
 yes | sudo apt install openssh-server
 yes | sudo apt install openssh-client
+sudo cp -f Ideals/RootLoginDenial.txt /etc/ssh/sshd_config #permit root login no
+sudo cp -f Ideals/SetLockout.txt /etc/pam.d/common-auth #lockout
 sudo bash IndivScripts/firefox_updater.sh
